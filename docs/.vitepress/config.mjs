94f1625eb30958345585626f4193202e725486a9
Haoze Zhang <iamzhz@foxmail.com>
2025-02-12 14:23:42 +0000

    3

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/text/",
   title: "the thing",
   description: "一个代替臃肿的美篇的东西",
   themeConfig: {
